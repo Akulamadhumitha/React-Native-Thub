a4d1024dff986e4f70a5e93d68ce2724749c11a2
Akulamadhumitha <akulamadhumita11@gmail.com>
2026-02-20 10:39:23 +0530

    add skills as chip list task

--- App.js
+++ App.js
@@ -1,37 +1,58 @@
-import {React} from "react";
+import {React, use} from "react";
 import { Text,StyleSheet,Image,FlatList, View} from "react-native";
-import { HelperText, TextInput } from "react-native-paper";
+import { HelperText, TextInput ,Button,Chip} from "react-native-paper";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
 
 const App = () => {
-  const Capture = (data) => {
-    const upper = /[A-Z]/.test(data);
-    const lower = /[a-z]/.test(data);
-    const specChar = /[^A-Za-z0-9]/.test(data);
-    const num = /[0-9]/.test(data);
-    const len = data.length>=8;
-    setstate(upper && lower && specChar && num && len)
-  };
-  const [state,setstate] = useState(false);
+  
+  const [chiplist,setchiplist] = useState([]);
+  const [text,settext]=useState("");
+  const [disable,setdisable]=useState(false);
+  const [helper,sethelper]=useState(true);
+  const Add = () => {
+    if(text.trim() === "") return;
 
+    setchiplist(prev => [...prev,text]);
+    settext("");
+  };
+  const Delete = (idx) => {
+    setchiplist(chiplist.filter((_,i) => i!==idx));
+  };
   return (
     <SafeAreaView style={{ flex: 1}}>
     <StatusBar style="dark" backgroundColor="green"/>
     
-    <TextInput 
-      onChangeText={(data)=>Capture(data)}
-      label={"password"}
-      error={!state}
-      mode="contained"
-      secureTextEntry
+    <TextInput
+      onChangeText={(data)=>settext(data)}
+      label={"Enter Skill"}
+      mode="view"
+      disabled={chiplist.length == 5 ? (!disable) : (disable)}
     />
-    <HelperText
-      
-    >
-    Enter input
+    <Button mode="contained" style={MyStyles.myButton} onPress={Add}
+      disabled={chiplist.length == 5 ? (!disable) : (disable)}
+
+    >Add Skill</Button>
+    <HelperText 
+    visible={chiplist.length > 0 ? (!helper) : (helper)}
+    style={MyStyles.helperText}>
+      No skills are added here.
     </HelperText>
+    {
+      chiplist.map((ele,idx)=>(
+        <View style={MyStyles.chipView}>
+        <Chip
+        // key={idx}
+        closeIcon="close-circle"
+        // onClose={()=>console.log("closed")}
+        onClose={()=>Delete(idx)}
+        >
+          {ele}
+        </Chip>
+        </View>
+      ))
+    }
 
   </SafeAreaView>
 )
@@ -40,5 +61,13 @@ const App = () => {
 export default App;
 
 const MyStyles = StyleSheet.create({
+     myButton:{
+      marginTop:10,
+      width:150,
+      marginLeft:"28%",
+     },
+     helperText:{
+      marginTop:10
+     }
      
 })
