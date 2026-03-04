4dc9f6ecf844aa2b16dde822b0b5280e8b3b23ab
Akulamadhumitha <akulamadhumita11@gmail.com>
2026-03-04 12:04:41 +0530

    add multiple images and delete task

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import React from "react"
 import { useState } from "react"
-import {Image,Text,View,StyleSheet} from "react-native"
-import { Button } from "react-native-paper"
+import {Image,Text,View,StyleSheet,FlatList} from "react-native"
+import { Button,IconButton } from "react-native-paper"
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context"
 import { StatusBar } from "expo-status-bar"
 import * as Imagepicker from "expo-image-picker"
 const App = () => {
-  const [ImagePath,setImagePath] = useState(null);
+  const [ImagePath,setImagePath] = useState([]);
   const FromGallery = async() => {
         const responce = await Imagepicker.requestMediaLibraryPermissionsAsync();
         if(!responce.granted){
@@ -14,10 +14,16 @@ const App = () => {
           return;
         }
         const data = await Imagepicker.launchImageLibraryAsync({
-          allowsEditing:true,
+          mediaTypes:"images",
+          allowsMultipleSelection:true,
+          allowsEditing:false,
           quality:1
         })
-        setImagePath(data.assets[0].uri);
+        setImagePath(data.assets);
+  };
+  const DeleteOption = (idx) => {
+      setImagePath((prevImages)=>
+        prevImages.filter((_,i)=>i!=idx))
   };
     return (
       <>
@@ -25,14 +31,38 @@ const App = () => {
         <StatusBar style="dark" backgroundColor="green"/>
 
         <View style={MyStyles.parentView}>
-          <Button mode="contained" onPress={FromGallery}>Chose from Gallery</Button>
+          <Button mode="contained" onPress={FromGallery}>ADD</Button>
         </View>
         {
-  !ImagePath ? (
-    <Text>No Image Selected!</Text>
-  ) : (
-    <Image source={{ uri: ImagePath }} style={MyStyles.imageStyling}/>
-  )
+        ImagePath.length > 0 ? (
+        <FlatList
+          data={ImagePath}
+          keyExtractor={(item)=>item.uri}
+          numColumns={2}
+          renderItem={({item,index})=>(
+            <View style={MyStyles.imageWrapper}>
+            <Image
+              source={{uri:item.uri}}
+              style={MyStyles.imageStyling}
+            />
+            <IconButton
+              icon="delete"
+              iconColor="white"
+              size={16}
+              style={MyStyles.deleteIcon}
+              onPress={()=>DeleteOption(index)}
+            />
+            </View>
+          )}
+          
+          // contentContainerStyle={{ padding: 10 }}
+        />
+      )
+        : (
+          <Text style={{ textAlign: "center", marginTop: 20 }}>
+      No Image Selected!
+    </Text>
+        )
 }
 
       </SafeAreaView>
@@ -50,10 +80,22 @@ const MyStyles = StyleSheet.create({
      marginLeft:"auto",
      marginRight:"auto"
   },
-  imageStyling:{
-     width: 200, 
-     height: 200,
-     marginLeft:20
-     
-  }
+  imageWrapper: {
+  width: 160,
+  height: 160,
+  margin: 10,
+},
+
+imageStyling: {
+  width: "100%",
+  height: "100%",
+  borderRadius: 10,
+},
+
+deleteIcon: {
+  position: "absolute",
+  top: 5,
+  right: 5,
+  backgroundColor: "rgba(0,0,0,0.6)"
+}
 })
