cf373551b1abc78f52f759d068df5f5f01036c2e
Akulamadhumitha <akulamadhumita11@gmail.com>
2026-03-05 11:12:08 +0530

    learned sharing single image

--- App.js
+++ App.js
@@ -5,65 +5,44 @@ import { Button,IconButton } from "react-native-paper"
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context"
 import { StatusBar } from "expo-status-bar"
 import * as Imagepicker from "expo-image-picker"
+import * as Sharing from "expo-sharing"
 const App = () => {
-  const [ImagePath,setImagePath] = useState([]);
-  const FromGallery = async() => {
-        const responce = await Imagepicker.requestMediaLibraryPermissionsAsync();
-        if(!responce.granted){
-          alert("Please give Access to me");
-          return;
-        }
-        const data = await Imagepicker.launchImageLibraryAsync({
-          mediaTypes:"images",
-          allowsMultipleSelection:true,
-          allowsEditing:false,
-          quality:1
-        })
-        setImagePath(data.assets);
+  const [ImagePath,setImagePath] = useState(null);
+  const [ShareBtn,setShareBtn] = useState(true);
+  const Choose = async() => {
+      const responce = await Imagepicker.getMediaLibraryPermissionsAsync();
+      if(!responce.granted){
+        alert("Not permitted!");
+        return;
+      }
+      const data = await Imagepicker.launchImageLibraryAsync({
+        mediaTypes:"images",
+        quality:1
+      });
+     setImagePath(data.assets[0].uri);
   };
-  const DeleteOption = (idx) => {
-      setImagePath((prevImages)=>
-        prevImages.filter((_,i)=>i!=idx))
+  const Share = async() => {
+      const status = await Sharing.isAvailableAsync();
+      if(!status){
+        alert("Not allowed to share");
+        return;
+      }
+      await Sharing.shareAsync(ImagePath)
   };
     return (
       <>
       <SafeAreaView style={{flex:1}}>
         <StatusBar style="dark" backgroundColor="green"/>
-
-        <View style={MyStyles.parentView}>
-          <Button mode="contained" onPress={FromGallery}>ADD</Button>
-        </View>
+       <View>
+        <Button onPress={Choose} mode="contained" style={{width:200,marginLeft:"auto",marginRight:"auto",marginTop:10,marginBottom:10}}>Choose Image</Button>
+       </View>
         {
-        ImagePath.length > 0 ? (
-        <FlatList
-          data={ImagePath}
-          keyExtractor={(item)=>item.uri}
-          numColumns={2}
-          renderItem={({item,index})=>(
-            <View style={MyStyles.imageWrapper}>
-            <Image
-              source={{uri:item.uri}}
-              style={MyStyles.imageStyling}
-            />
-            <IconButton
-              icon="delete"
-              iconColor="white"
-              size={16}
-              style={MyStyles.deleteIcon}
-              onPress={()=>DeleteOption(index)}
-            />
-            </View>
-          )}
-          
-          // contentContainerStyle={{ padding: 10 }}
-        />
-      )
-        : (
-          <Text style={{ textAlign: "center", marginTop: 20 }}>
-      No Image Selected!
-    </Text>
-        )
-}
+          ImagePath ?
+          (<Image source={{uri:ImagePath}} style={{width:200,height:200}}/>)
+          :
+          (<Text style={{textAlign:"center",padding:20}}>No Image Selected!</Text>)
+        }
+        <Button onPress={Share} disabled={ ImagePath ? !ShareBtn : ShareBtn} style={{ width:100,marginLeft:"auto",marginRight:"auto",marginTop:10,marginBottom:10}} mode="contained">Share</Button>
 
       </SafeAreaView>
 
@@ -72,30 +51,30 @@ const App = () => {
 }
 export default App;
 
-const MyStyles = StyleSheet.create({
-  parentView:{
-     padding:10,
-     width:260,
-     marginTop:10,
-     marginLeft:"auto",
-     marginRight:"auto"
-  },
-  imageWrapper: {
-  width: 160,
-  height: 160,
-  margin: 10,
-},
+// const MyStyles = StyleSheet.create({
+//   parentView:{
+//      padding:10,
+//      width:260,
+//      marginTop:10,
+//      marginLeft:"auto",
+//      marginRight:"auto"
+//   },
+//   imageWrapper: {
+//   width: 160,
+//   height: 160,
+//   margin: 10,
+// },
 
-imageStyling: {
-  width: "100%",
-  height: "100%",
-  borderRadius: 10,
-},
+// imageStyling: {
+//   width: "100%",
+//   height: "100%",
+//   borderRadius: 10,
+// },
 
-deleteIcon: {
-  position: "absolute",
-  top: 5,
-  right: 5,
-  backgroundColor: "rgba(0,0,0,0.6)"
-}
-})
+// deleteIcon: {
+//   position: "absolute",
+//   top: 5,
+//   right: 5,
+//   backgroundColor: "rgba(0,0,0,0.6)"
+// }
+// })
