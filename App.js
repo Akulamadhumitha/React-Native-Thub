9cb97a81239866a4b2f25fa41d1289fbf199283c
Akulamadhumitha <akulamadhumitha11@gmail.com>
2026-03-24 10:48:10 +0530

    Notifications basics learned

--- App.js
+++ App.js
@@ -4,22 +4,31 @@ import {Image,Text,View,StyleSheet,FlatList} from "react-native"
 import { Button,IconButton } from "react-native-paper"
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context"
 import { StatusBar } from "expo-status-bar"
-import * as LocalAuthentication from "expo-local-authentication"
+import * as Notifications from "expo-notifications";
+Notifications.setNotificationHandler({
+  handleNotification:async()=>({
+    shouldPlaySound:true,
+    shouldShowBanner:true,
+    // shouldSetBadge:true
+  })
+})
 const App = () => {
-    
-  const Open = async() => {
-     const status = await LocalAuthentication.hasHardwareAsync();
-     console.log("hardware",status);
-     const isEnrolled = await LocalAuthentication.isEnrolledAsync();
-     console.log("enrolled",isEnrolled);
-     const result = await LocalAuthentication.authenticateAsync();
-  }
+    const GetToken = async() => {
+       const permission = await Notifications.getPermissionsAsync();
+       if(!permission.granted){
+        await Notifications.requestPermissionsAsync();
+        return;
+       }
+       const Data = Notifications.getExpoPushTokenAsync();
+       console.log(Data);
+    };
+  
     return (
       <>
       <SafeAreaView style={{flex:1}}>
         <StatusBar style="dark" backgroundColor="green"/>
        <View>
-         <Button onPress={Open}>Click</Button>
+         <Button onPress={GetToken}>Get Notified</Button>
         </View>
       </SafeAreaView>
 
